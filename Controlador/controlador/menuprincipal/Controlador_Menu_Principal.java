6b6a02b6fcd6fa6e9b72896cb1473d1421ade7c6
Rodrigo <rodrigoramil@hotmail.es>
2022-05-06 03:34:01 +0200

    añadiendo reestucturando la clase Vista_Login, añadiendo comportamiento lógico a los botones y criptografía de la contraseña

---
+++ Controlador/controlador/menuprincipal/Controlador_Menu_Principal.java
@@ -0,0 +1,52 @@
+package controlador.menuprincipal;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import vista.menuprincipal.Vista_Menu_Principal;
+
+public class Controlador_Menu_Principal implements ActionListener {
+	
+	
+	
+	private Vista_Menu_Principal vistaMenuPrincipal;
+
+
+
+	public Controlador_Menu_Principal(Vista_Menu_Principal vista_Menu_Principal) {
+		this.vistaMenuPrincipal = vista_Menu_Principal;
+	}
+
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == vistaMenuPrincipal.getBtn_Ventas()) {
+			
+		}
+		
+		if (e.getSource() == vistaMenuPrincipal.getBtn_Recetario()) {
+			
+		}
+		
+		if (e.getSource() == vistaMenuPrincipal.getBtn_Almacen()) {
+			
+		}
+		
+		if (e.getSource() == vistaMenuPrincipal.getBtn_Contabilidad()) {
+			
+		}
+		
+		if (e.getSource() == vistaMenuPrincipal.getBtn_Gestion_Usuario()) {
+			
+		}
+		
+		if (e.getSource() == vistaMenuPrincipal.getBtn_Cerrar_Sesion()) {
+			
+		}
+	
+	
+	
+	}
+
+}
--- Vista/vista/login/Vista_Login.java
+++ Vista/vista/login/Vista_Login.java
@@ -1,8 +1,5 @@
 package vista.login;
 
-import java.awt.Color;
-import java.awt.Font;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,75 +8,173 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import controlador.login.Controlador_Login;
+
 public class Vista_Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	
+	private static JPanel contentPane;
+	private JLabel lbl_Usuario;
+	private JLabel lbl_Contraseña;
 	private JTextField jtf_Entrada_Nombre;
 	private JPasswordField jtf_Entrada_Contrasena;
+	private JButton btn_Aceptar;
+	private JButton btn_Borrar;
+	private static String tituloVentana = "Comidas La Natural - ";
+	private int ancho = 450;
+	private int alto = 300;
 
 	public Vista_Login() {
-		setResizable(false);
-		setTitle("Login");
+		
+		
+		setResizable(false); // fina el tamaño de la ventana
+		setTitle(tituloVentana + "Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(ancho, alto);	// tamaño de la ventana
+		setLocationRelativeTo(null); // ventana en el centro de la pantalla
+		getContentPane().setLayout(null); // desactiva el organizador de objetos de la ventana
+//		setIconImage(Toolkit.getDefaultToolkit().getImage("img/imagenicono.jpg"));
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btn_Aceptar = new JButton("Aceptar");
+		inicializarComponentes();
+		setVisible(true);
+		establecerManejador();
+	}
+
+
+	public void inicializarComponentes() {
+		
+
+		
+		btn_Aceptar = new JButton("Aceptar");
 	
-		btn_Aceptar.setBounds(93, 198, 89, 23);
+		btn_Aceptar.setBounds(93, 200, 90, 23);
 		contentPane.add(btn_Aceptar);
 		
-		JButton btn_Borrar = new JButton("Borrar");
-		btn_Borrar.setBounds(246, 198, 89, 23);
+		btn_Borrar = new JButton("Borrar");
+		btn_Borrar.setBounds(246, 200, 90, 23);
 		contentPane.add(btn_Borrar);
 		
 		jtf_Entrada_Nombre = new JTextField();
-		jtf_Entrada_Nombre.setBounds(139, 81, 86, 20);
+		jtf_Entrada_Nombre.setBounds(190, 80, 100, 20);
 		contentPane.add(jtf_Entrada_Nombre);
 		jtf_Entrada_Nombre.setColumns(10);
 		
 		jtf_Entrada_Contrasena = new JPasswordField();
-		jtf_Entrada_Contrasena.setBounds(139, 123, 86, 20);
+		jtf_Entrada_Contrasena.setBounds(190, 120, 100, 20);
 		contentPane.add(jtf_Entrada_Contrasena);
 		
-		JLabel lbl_Error_Usuario = new JLabel("Usuario incorrecto");
-		lbl_Error_Usuario.setEnabled(false);
-		lbl_Error_Usuario.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lbl_Error_Usuario.setForeground(Color.RED);
-		lbl_Error_Usuario.setBounds(252, 84, 126, 14);
-		contentPane.add(lbl_Error_Usuario);
-		
-		JLabel lbl_Error_Contrasena = new JLabel("Contrase\u00F1a incorrecta");
-		lbl_Error_Contrasena.setEnabled(false);
-		lbl_Error_Contrasena.setForeground(Color.RED);
-		lbl_Error_Contrasena.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lbl_Error_Contrasena.setBounds(252, 126, 126, 14);
-		contentPane.add(lbl_Error_Contrasena);
-		
-		JLabel lbl_Usuario = new JLabel("Usuario");
-		lbl_Usuario.setBounds(53, 84, 66, 14);
+		lbl_Usuario = new JLabel("Usuario");
+		lbl_Usuario.setBounds(105, 83, 66, 14);
 		contentPane.add(lbl_Usuario);
 		
-		JLabel lbl_Contraseña = new JLabel("Contrase\u00F1a");
-		lbl_Contraseña.setBounds(53, 126, 66, 14);
+		lbl_Contraseña = new JLabel("Contrase\u00F1a");
+		lbl_Contraseña.setBounds(105, 125, 66, 14);
 		contentPane.add(lbl_Contraseña);
 	}
 	
-public void establecerManejador() {
-		
-		
-//		Controlador_menu_principal controlador = new Controlador_menu_principal(this);
-//
-//		btn_Aceptar.addActionListener(controlador);
-//		boton_principal_recetario.addActionListener(controlador);
-//		boton_principal_almacen.addActionListener(controlador);
-//		boton_principal_contabilidad.addActionListener(controlador);
-//		boton_principal_usuario.addActionListener(controlador);
-//		boton_principal_salir.addActionListener(controlador);
+	public void establecerManejador() {			
+			Controlador_Login controlador = new Controlador_Login(this);	
+			btn_Aceptar.addActionListener(controlador);
+			btn_Borrar.addActionListener(controlador);
 		
 	}
+
+
+
+	public JLabel getLbl_Usuario() {
+		return lbl_Usuario;
+	}
+
+
+	public void setLbl_Usuario(JLabel lbl_Usuario) {
+		this.lbl_Usuario = lbl_Usuario;
+	}
+
+
+	public JLabel getLbl_Contraseña() {
+		return lbl_Contraseña;
+	}
+
+
+	public void setLbl_Contraseña(JLabel lbl_Contraseña) {
+		this.lbl_Contraseña = lbl_Contraseña;
+	}
+
+
+	public JTextField getJtf_Entrada_Nombre() {
+		return jtf_Entrada_Nombre;
+	}
+
+
+	public void setJtf_Entrada_Nombre(JTextField jtf_Entrada_Nombre) {
+		this.jtf_Entrada_Nombre = jtf_Entrada_Nombre;
+	}
+
+
+	public JPasswordField getJtf_Entrada_Contrasena() {
+		return jtf_Entrada_Contrasena;
+	}
+
+
+	public void setJtf_Entrada_Contrasena(JPasswordField jtf_Entrada_Contrasena) {
+		this.jtf_Entrada_Contrasena = jtf_Entrada_Contrasena;
+	}
+
+
+	public JButton getBtn_Aceptar() {
+		return btn_Aceptar;
+	}
+
+
+	public void setBtn_Aceptar(JButton btn_Aceptar) {
+		this.btn_Aceptar = btn_Aceptar;
+	}
+
+
+	public JButton getBtn_Borrar() {
+		return btn_Borrar;
+	}
+
+
+	public void setBtn_Borrar(JButton btn_Borrar) {
+		this.btn_Borrar = btn_Borrar;
+	}
+
+	public static String getTituloVentana() {
+		return tituloVentana;
+	}
+
+
+	public static void setTituloVentana(String tituloVentana) {
+		Vista_Login.tituloVentana = tituloVentana;
+	}
+
+
+	public int getAncho() {
+		return ancho;
+	}
+
+
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+
+
+	public int getAlto() {
+		return alto;
+	}
+
+
+	public void setAlto(int alto) {
+		this.alto = alto;
+	}
+
+
+
 }
