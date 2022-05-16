e64d3558ef5ab1f2b35ed4d063f234cc80caf648
wonderlex <44850310+wonderlex@users.noreply.github.com>
2022-05-16 10:24:40 +0200

    Implementando pedidos a falta del IdCliente

--- src/modelo/ModeloPedido.java
+++ src/modelo/ModeloPedido.java
@@ -4,24 +4,22 @@ import java.io.Serializable;
 
 public class ModeloPedido implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -2144175894579284069L;
 	
 	String comidaBebida;
-	String cantidad;
-	String precio;
-	
-	public ModeloPedido(String comidaBebida, String cantidad, String precio) {
+	int cantidad;
+	float precio;
+	int IdCliente;
+	public ModeloPedido(String comidaBebida, int cantidad, float precio) {
 		super();
 		this.comidaBebida = comidaBebida;
 		this.cantidad = cantidad;
 		this.precio = precio;
 	}
 
-	public ModeloPedido() {
+	public ModeloPedido(String string, float f, int i, String string2) {
 		super();
+		this.IdCliente = IdCliente;
 	}
 
 	public String getComidaBebida() {
@@ -32,19 +30,19 @@ public class ModeloPedido implements Serializable {
 		this.comidaBebida = comidaBebida;
 	}
 
-	public String getCantidad() {
+	public int getCantidad() {
 		return cantidad;
 	}
 
-	public void setCantidad(String cantidad) {
+	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
 
-	public String getPrecio() {
+	public float getPrecio() {
 		return precio;
 	}
 
-	public void setPrecio(String precio) {
+	public void setPrecio(float precio) {
 		this.precio = precio;
 	}
 
